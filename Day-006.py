a2812c98bab1df7d4462f7652647986adb907569
Girig <srigirit369@gmail.comm>
2022-08-12 20:56:56 +0530

    Day-006.py

---
+++ Day-006.py
@@ -0,0 +1,67 @@
+# Reeborg's world
+
+# challenge before last one
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# def hu():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+#
+# while (not at_goal()):
+#     if (front_is_clear()):
+#         move()
+#     elif (wall_in_front()):
+#         hu()
+
+# Final Challenge of Hurdle
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# def hu():
+#     turn_left()
+#     move()
+#     while (wall_on_right()):
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     while (not wall_in_front()):
+#         move()
+#     turn_left()
+#
+#
+# while (not at_goal()):
+#     if (front_is_clear()):
+#         move()
+#     elif (wall_in_front()):
+#         hu()
+
+# Final challenge of the day
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# while (not at_goal()):
+#     if (right_is_clear()):
+#         turn_right()
+#         move()
+#     elif (wall_on_right() and not wall_in_front()):
+#         move()
+#     else:
+#         turn_left()
