907122101b5a1639f42b2a3f2ea7d8491b538559
SriGiriT <srigirit369@gmail.com>
2022-08-13 18:59:56 +0530

    Day-007.py

---
+++ Day-007.py
@@ -0,0 +1,86 @@
+import random
+
+hang = ["""
+   +---+
+   |   |
+       |
+       |
+       |
+       |
+=========""", """
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========""", """
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========""", """
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========""", """
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========""", """
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========""", """
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+========="""]
+words = ["random", "words", "for", "hangman", "game", "was", "kind", "of", "cool"]
+word = random.choice(words)
+c = 0
+ans = ["_" for i in range(len(word))]
+for j in ans:
+    print(j, end="")
+print()
+print("Guess the word")
+while "_" in ans:
+    inp = input()
+    if(inp in word and inp not in ans):
+        print(hang[c])
+        for i in range(len(word)):
+            if inp == word[i]:
+                ans[i] = word[i]
+        for j in ans:
+            print(j, end="")
+        print()
+    elif(inp in ans):
+        print("Already Guessed !!!")
+        print(hang[c])
+    else:
+        if(c < 6 ):
+            print(hang[c])
+            c+=1
+            for j in ans:
+                print(j, end="")
+            print()
+        else:
+            print(hang[c])
+            print("you lose ")
+            break
+if c < 6:
+    print("You win!!!")
