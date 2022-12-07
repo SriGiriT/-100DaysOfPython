8e3b4df7dcce63b1ec6cf90cb7789eba3f3bc496
SriGiriT <srigirit369@gmail.com>
2022-12-07 19:41:19 +0530

    Day-042.py

---
+++ Day-042.py/Day-042.py
@@ -0,0 +1,15 @@
+
+
+
+import requests
+from datetime import datetime
+
+pixela_endpoint = "https://pixe.la/v1/users"
+Token = "G@$3G@$3G@$3G@$3"
+Username =  "srigirit"
+user_params = {
+    "token" : Token,
+    "username": Username,
+    "agreeTermsOfService": "yes", 
+    "notMinor": "yes"
+}
